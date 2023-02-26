0a8568431a5c62b11e5939aea155ee2f1ad18420
mathbrahian <mathbrahian@gmail.com>
2023-02-26 16:02:00 -0500

    solucion usando imagen de tiangolo

--- server/app/main.py
+++ server/app/main.py
@@ -34,4 +34,4 @@ class ApiUploadPhoto(Resource):
 api.add_resource(ApiUploadPhoto, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=80)
